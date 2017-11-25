62520f72c57178301d9f614cbe267aabc687d6be
datawine <sjx15@mails.tsinghua.edu.cn>
2017-11-26 01:18:35 +0800

    test

--- pc.vhd
+++ pc.vhd
@@ -48,11 +48,11 @@ begin
 	change_pc_register: process(clk) is
 	begin
 		if (clk'event and clk = '1') then
---			if (pc_staller = '0') then
+			if (pc_staller = '0') then
 				pc_register <= pc_in;
 --			else
 --				pc_register <= unaffected;
---			end if;
+			end if;
 		end if;
 	end process;
 
